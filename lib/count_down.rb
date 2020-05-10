78781432288c2e2bad4f3f4f7d9f5d56c74cf073
Leamsi Escribano <leamsii.e@gmail.com>
2020-05-10 22:59:45 +0000

    Done.

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,1 +1,17 @@
 # Write your code here
+
+counter = 10
+while counter != 0 do
+  puts counter
+  counter -= 1
+end
+puts "Happy New Year!"
+
+counter = 0
+while counter < 100 do
+  counter += 1
+  if counter == 3
+    break
+  end
+end
+
