e6f6be561ca8c6e14eb99db14cdccc29653c7402
Leamsi Escribano <leamsii.e@gmail.com>
2020-05-10 22:57:22 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -4,3 +4,15 @@ counter = 0
 while counter < 10 do
   counter += 1
 end
+
+counter = 0
+while counter < 100 do
+  counter += 1
+  if counter == 3
+    break
+  end
+end
+
+while true do
+  puts "AHHHHHH"
+end
